32c5c00e53bb168cf7b58ac0bfc49ac3bdc162f3
kishorek <kishorek@entoo.com>
2024-04-07 08:31:22 +0530

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,114 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
+
+const Contact = () => {
+
+  const formRef = useRef();
+
+  const [form, setForm] = useState({ name: '', email: '', message: '' })
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { alert, showAlert, hideAlert } = useAlert();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: 'Tanuja K',
+        from_email: form.email,
+        to_email: 'defetron@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+    ).then(() => {
+      showAlert({
+        show: true, text: 'Message sent successfully!',
+        type: 'success'
+      })
+      setForm({ name: '', email: '', message: '' })
+      setIsLoading(false);
+      setTimeout(() => {
+        hideAlert();
+      }, 3000);
+    }).catch((error) => {
+      console.log(error);
+      showAlert({
+        show: true, text: "I didn't receive your message",
+        type: 'danger'
+      })
+      setIsLoading(false);
+      setTimeout(() => {
+        hideAlert();
+      }, 3000);
+    })
+  }
+
+  return (
+    <section className='relative flex flex-col max-container h-full'>
+      {alert.show && <Alert {...alert}/>}
+      <div className='mt-2 flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+        <form className='w-full flex flex-col gap-7 mt-14'
+          onSubmit={handleSubmit}
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='Tanu'
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='Tanu@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows={4}
+              className='textarea'
+              placeholder='Let me know how I can help you!'
+              required
+              value={form.message}
+              onChange={handleChange}
+            />
+          </label>
+          <button
+            type="submit"
+            className='btn'
+            disabled={isLoading}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from './Home'
+import Projects from './Projects'
+import Contact from './Contact'
+
+export { Home, Projects, Contact }
